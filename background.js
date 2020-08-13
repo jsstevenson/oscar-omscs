cbf2b6984569ea252f7f2fb975e89fa54bb3a74d
James Stevenson <james.sharpsteen@gmail.com>
2020-08-13 11:14:43 -0400

    Remove debugging print statements

--- background.js
+++ background.js
@@ -1,15 +1,12 @@
 function handleClick(tab) {
 
     function onExecuted(result) {
-        console.log(`We executed in all subframes`);
+        console.log(`Executed in all subframes`);
     }
 
     function onError(error) {
         console.log(`Error: ${error}`);
     }
-    // execute content script
-    console.log("hello");
-
     let executing = browser.tabs.executeScript({
         file: "/content-script.js",
         allFrames: true
