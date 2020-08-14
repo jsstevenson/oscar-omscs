4ae6a79e1fb7b11069878cb349e30bd0bcf12fda
James Stevenson <james.sharpsteen@gmail.com>
2020-08-14 09:02:39 -0400

    Add class URLs

--- content-script.js
+++ content-script.js
@@ -5,126 +5,247 @@
  */
 let data = {
     "cse-6220-01": {
+        "url": "https://omscs.gatech.edu/cse-6220-intro-hpc",
+        "history": {
+        }
     },
     "cse-6242-01": {
-        "spring-2020": [400, 399]
+        "url": "https://omscs.gatech.edu/cse-6242-data-visual-analytics",
+        "history": {
+            "spring-2020": [400, 399]
+        }
     },
     "cse-6250-01": {
-        "spring-2020": [300, 247]
+        "url": "https://omscs.gatech.edu/cse-8803-special-topics-big-data-for-health-informatics",
+        "history": {
+            "spring-2020": [300, 247]
+        }
     },
     "cs-6035-01": {
-        "spring-2020": [700, 663]
+        "url": "https://omscs.gatech.edu/cs-6035-introduction-to-information-security",
+        "history": {
+            "spring-2020": [700, 663]
+        }
     },
     "cs-6200-01": {
-        "spring-2020": [800, 700]
+        "url": "https://omscs.gatech.edu/cs-6200-introduction-operating-systems",
+        "history": {
+            "spring-2020": [800, 700]
+        }
     },
     "cs-6210-01": {
-        "spring-2020": [300, 257]
+        "url": "https://omscs.gatech.edu/cs-6210-advanced-operating-systems",
+        "history": {
+            "spring-2020": [300, 257]
+        }
     },
     "cs-6238-01": {
-        "spring-2020": [200, 103]
+        "history": {
+            "spring-2020": [200, 103]
+        }
     },
     "cs-6250-01": {
-        "spring-2020": [600, 599]
+        "url": "https://omscs.gatech.edu/cs-6250-computer-networks",
+        "history": {
+            "spring-2020": [600, 599]
+        }
     },
     "cs-6260-01": {
-        "spring-2020": [75, 72]
+        "history": {
+            "spring-2020": [75, 72]
+        }
     },
     "cs-6262-01": {
-        "spring-2020": [250, 57]
+        "url": "https://omscs.gatech.edu/cs-6262-network-security",
+        "history": {
+            "spring-2020": [250, 57]
+        }
     },
     "cs-6263-01": {
-        "spring-2020": [300, 260]
+        "url": "https://omscs.gatech.edu/cs-8803-special-topics-cyber-physical-systems",
+        "history": {
+            "spring-2020": [300, 260]
+        }
     },
     "cs-6265-01": {
-        "spring-2020": [200, 40]
+        "url": "https://omscs.gatech.edu/cs-6265-information-security-lab",
+        "history": {
+            "spring-2020": [200, 40]
+        }
     },
     "cs-6290-01": {
-        "spring-2020": [350, 265]
+        "url": "https://omscs.gatech.edu/cs-6290-high-performance-computer-architecture",
+        "history": {
+            "spring-2020": [350, 265]
+        }
     },
     "cs-6291-01": {
-        "spring-2020": [200, 83]
+        "url": "https://omscs.gatech.edu/cs-6291-embedded-systems-optimization",
+        "history": {
+            "spring-2020": [200, 83]
+        }
     },
     "cs-6300-01": {
-        "spring-2020": [500, 500]
+        "url": "https://omscs.gatech.edu/cs-6300-software-development-process",
+        "history": {
+            "spring-2020": [500, 500]
+        }
     },
     "cs-6310-01": {
-        "spring-2020": [600, 538]
+        "url": "https://omscs.gatech.edu/cs-6310-software-architecture-design",
+        "history": {
+            "spring-2020": [600, 538]
+        }
     },
     "cs-6340-01": {
-        "spring-2020": [400, 347]
+        "url": "https://omscs.gatech.edu/cs-6340-software-analysis",
+        "history": {
+            "spring-2020": [400, 347]
+        }
     },
     "cs-6400-01": {
-        "spring-2020": [500, 462]
+        "url": "https://omscs.gatech.edu/cs-6400-database-systems-concepts-and-design",
+        "history": {
+            "spring-2020": [500, 462]
+        }
     },
     "cs-6440-01": {
-        "spring-2020": [200, 200]
+        "url": "https://omscs.gatech.edu/cs-6440-intro-health-informatics",
+        "history": {
+            "spring-2020": [200, 200]
+        }
     },
     "cs-6457-01": {
+        "url": "https://omscs.gatech.edu/cs-6457-video-game-design"
+        "history": {}
     },
     "cs-6460-01": {
-        "spring-2020": [250, 220]
+        "url": "https://omscs.gatech.edu/cs-6460-educational-technology",
+        "history": {
+            "spring-2020": [250, 220]
+        }
     },
     "cs-6475-01": {
-        "spring-2020": [400, 323]
+        "url": "https://omscs.gatech.edu/cs-6475-computational-photography",
+        "history": {
+            "spring-2020": [400, 323]
+        }
     },
     "cs-6476-01": {
-        "spring-2020": [400, 400]
+        "url": "https://omscs.gatech.edu/cs-6476-computer-vision",
+        "history": {
+            "spring-2020": [400, 400]
+        }
     },
     "cs-6515-01": {
-        "spring-2020": [526,  527]
+        "url": "https://omscs.gatech.edu/cs-6515-intro-graduate-algorithms",
+        "history": {
+            "spring-2020": [526,  527]
+        }
     },
     "cs-6601-01": {
-        "spring-2020": [813, 37]
+        "url": "https://omscs.gatech.edu/cs-6601-artificial-intelligence",
+        "history": {
+            "spring-2020": [813, 37]
+        }
     },
     "cs-6750-01": {
-        "spring-2020": [500, 444]
+        "url": "https://omscs.gatech.edu/cs-6750-human-computer-interaction",
+        "history": {
+            "spring-2020": [500, 444]
+        }
     },
     "cs-7637-01": {
-        "spring-2020": [650, 584]
+        "url": "https://omscs.gatech.edu/cs-7637-knowledge-based-artificial-intelligence-cognitive-systems",
+        "history": {
+            "spring-2020": [650, 584]
+        }
     },
     "cs-7638-01": {
-        "spring-2020": [700, 638]
+        "url": "https://omscs.gatech.edu/cs-7638-artificial-intelligence-robotics",
+        "history": {
+            "spring-2020": [700, 638]
+        }
     },
     "cs-7639-01": {
-        "spring-2020": [200, 83]
+        "url": "https://omscs.gatech.edu/cs-7639-cyber-physical-design-and-analysis",
+        "history": {
+            "spring-2020": [200, 83]
+        }
     },
     "cs-7641-01": {
-        "spring-2020": [750, 750]
+        "url": "https://omscs.gatech.edu/cs-7641-machine-learning",
+        "history": {
+            "spring-2020": [750, 750]
+        }
     },
     "cs-7642-03": {
-        "spring-2020": [572, 503]
+        "url": "https://omscs.gatech.edu/cs-7642-reinforcement-learning",
+        "history": {
+            "spring-2020": [572, 503]
+        }
     },
     "cs-7643-01": {
+        "url": "https://omscs.gatech.edu/cs-7643-deep-learning",
+        "history": {}
+        }
     },
     "cs-7646-01": {
-        "spring-2020": [700, 700]
+        "url": "https://omscs.gatech.edu/cs-7643-deep-learning",
+        "history": {
+            "spring-2020": [700, 700]
+        }
     },
     "cs-8803-08": {
-        "spring-2020": [200, 48]
+        "url": "https://omscs.gatech.edu/cs-8803-o08-compilers-theory-and-practice",
+        "history": {
+            "spring-2020": [200, 48]
+        }
     },
     "cs-8803-10": {
-        "spring-2020": [100, 100]
+        "url": "https://omscs.gatech.edu/cs-8803-o10-ai-ethics-and-society",
+        "history": {
+            "spring-2020": [100, 100]
+        }
     },
     "cs-8803-11": {
-        "spring-2020": [100, 22]
+        "url": "https://omscs.gatech.edu/cs-8803-o11-information-security-lab-system-and-network-defenses",
+        "history": {
+            "spring-2020": [100, 22]
+        }
     },
     "isye-6402-01": {
-        "spring-2020": [200, 42]
+        "url": "https://omscs.gatech.edu/isye-6402-time-series-analysis",
+        "history": {
+            "spring-2020": [200, 42]
+        }
     },
     "isye-6420-01": {
-        "spring-2020": [200, 200]
+        "url": "http://www.omscs.gatech.edu/isye-6420-bayesian-statistics",
+        "history": {
+            "spring-2020": [200, 200]
+        }
     },
     "isye-6501-01": {
-        "spring-2020": [300, 249]
+        "url": "https://omscs.gatech.edu/isye-6501-intro-analytics-modeling",
+        "history": {
+            "spring-2020": [300, 249]
+        }
     },
     "isye-6644-01": {
-        "spring-2020": [200, 186]
+        "history": {
+            "spring-2020": [200, 186]
+        }
     },
     "isye-8803-01": {
+        "url": "https://omscs.gatech.edu/isye-8803-topics-high-dimensional-data-analytics",
+        "history": {}
     },
     "pubp-6725-01": {
-        "spring-2020": [100, 48]
+        "url": "https://omscs.gatech.edu/pubp-6725-information-security-policies",
+        "history": {
+            "spring-2020": [100, 48]
+        }
     }
 }
 
@@ -132,7 +253,7 @@ let data = {
 let re = /O[0-9][0-9]/;
 
 /* Checks if provided row (<tr> element) is an OMSCS course or not
- */
+*/
 function isOMSCS(row) {
     if (row.children[5].textContent != "O") {
         return false;
@@ -144,7 +265,7 @@ function isOMSCS(row) {
 }
 
 /* Add color to course registration numbers
- */
+*/
 function colorRow(row) {
     let act = parseInt(row.children[12].textContent);
     let cap = parseInt(row.children[11].textContent);
@@ -183,6 +304,15 @@ function iterateTable(rows) {
                 colorRow(row);
                 let courseName = `${row.children[2].textContent}-${row.children[3].textContent}-${row.children[4].textContent.slice(-2)}`.toLowerCase();
                 row.id = courseName;
+                if (data[courseName]["url"]) {
+                    let link = document.createElement("a");
+                    link.href = data[courseName]["url"];
+                    link.textContent = row.children[8].textContent;
+                    link.setAttribute("target", "_blank");
+                    link.setAttribute("rel", "noopener noreferrer");
+                    row.children[8].textContent = "";
+                    row.children[8].appendChild(link);
+                }
             }
         }
         if (row.children.length > 19) {
